f0d2a578e5ad543f4aa52c6cbff8d7975213341c
suizoperuano <andrechedel@hotmail.com>
2023-02-09 20:59:13 +0000

    lastlast

--- src/app.js
+++ src/app.js
@@ -17,28 +17,38 @@ window.onload = function() {
     "K",
     "A"
   ];
+
+  let cardStack = [];
+  for (let i = 0; i < symbolArray.length; i++) {
+    for (let j = 0; j < cardNumbers.length; j++) {
+      cardStack.push([cardNumbers[j], symbolArray[i]]);
+    }
+  }
+
   let getRandom = myArray => {
     return myArray[Math.floor(Math.random() * myArray.length)];
   };
 
-  let randomSymbol = getRandom(symbolArray);
-  let randomNumber = getRandom(cardNumbers);
+  let randomCard = getRandom(cardStack);
 
-  console.log(randomNumber + randomSymbol);
+  console.log("picked card: " + randomCard[0] + randomCard[1]);
 
-  let topSymbol = document.querySelector(".symbol-top");
-  let bottomSymbol = document.querySelector(".symbol-bottom");
-  let cardNumber = document.querySelector(".card-number");
+  let suit = randomCard[1];
+  let cardNumber = randomCard[0];
+
+  let topSymbolHTML = document.querySelector(".symbol-top");
+  let bottomSymbolHTML = document.querySelector(".symbol-bottom");
+  let cardNumberHTML = document.querySelector(".card-number");
 
   let symbolColor = "black";
 
-  if (randomSymbol === "♥" || randomSymbol === "♦") {
+  if (suit === "♥" || suit === "♦") {
     symbolColor = "red";
   }
 
-  cardNumber.innerHTML = randomNumber;
-  topSymbol.innerHTML = randomSymbol;
-  bottomSymbol.innerHTML = randomSymbol;
-  topSymbol.style.color = symbolColor;
-  bottomSymbol.style.color = symbolColor;
+  cardNumberHTML.innerHTML = cardNumber;
+  topSymbolHTML.innerHTML = suit;
+  bottomSymbolHTML.innerHTML = suit;
+  topSymbolHTML.style.color = symbolColor;
+  bottomSymbolHTML.style.color = symbolColor;
 };
